be7f68657b499faf7e1487f9779c7308ee33e602
derekwong95 <WongDerekx@hotmail.com>
2019-01-01 15:38:41 -0800

    initial commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace MarketAnalyzer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.labelApiName = new System.Windows.Forms.Label();
+            this.textBoxApiName = new System.Windows.Forms.TextBox();
+            this.buttonSubmit = new System.Windows.Forms.Button();
+            this.listViewData = new System.Windows.Forms.ListView();
+            this.chartData = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelApiName
+            // 
+            this.labelApiName.AutoSize = true;
+            this.labelApiName.Location = new System.Drawing.Point(15, 14);
+            this.labelApiName.Name = "labelApiName";
+            this.labelApiName.Size = new System.Drawing.Size(53, 13);
+            this.labelApiName.TabIndex = 0;
+            this.labelApiName.Text = "Api Name";
+            this.labelApiName.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBoxApiName
+            // 
+            this.textBoxApiName.Location = new System.Drawing.Point(13, 36);
+            this.textBoxApiName.Name = "textBoxApiName";
+            this.textBoxApiName.Size = new System.Drawing.Size(314, 20);
+            this.textBoxApiName.TabIndex = 1;
+            this.textBoxApiName.TextChanged += new System.EventHandler(this.textBoxApiName_TextChanged);
+            // 
+            // buttonSubmit
+            // 
+            this.buttonSubmit.Location = new System.Drawing.Point(13, 62);
+            this.buttonSubmit.Name = "buttonSubmit";
+            this.buttonSubmit.Size = new System.Drawing.Size(80, 23);
+            this.buttonSubmit.TabIndex = 3;
+            this.buttonSubmit.Text = "Submit";
+            this.buttonSubmit.UseVisualStyleBackColor = true;
+            this.buttonSubmit.Click += new System.EventHandler(this.buttonSubmit_Click);
+            // 
+            // listViewData
+            // 
+            this.listViewData.Location = new System.Drawing.Point(13, 91);
+            this.listViewData.Name = "listViewData";
+            this.listViewData.Size = new System.Drawing.Size(314, 350);
+            this.listViewData.TabIndex = 4;
+            this.listViewData.UseCompatibleStateImageBehavior = false;
+            // 
+            // chartData
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chartData.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chartData.Legends.Add(legend1);
+            this.chartData.Location = new System.Drawing.Point(343, 36);
+            this.chartData.Name = "chartData";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chartData.Series.Add(series1);
+            this.chartData.Size = new System.Drawing.Size(899, 405);
+            this.chartData.TabIndex = 5;
+            this.chartData.Text = "chart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1266, 699);
+            this.Controls.Add(this.chartData);
+            this.Controls.Add(this.listViewData);
+            this.Controls.Add(this.buttonSubmit);
+            this.Controls.Add(this.textBoxApiName);
+            this.Controls.Add(this.labelApiName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chartData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelApiName;
+        private System.Windows.Forms.TextBox textBoxApiName;
+        private System.Windows.Forms.Button buttonSubmit;
+        private System.Windows.Forms.ListView listViewData;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartData;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using AlphaVantageApiWrapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MarketAnalyzer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void buttonSubmit_Click(object sender, EventArgs e)
+        {
+            var API_KEY = textBoxApiName.Text;
+
+            var StockTickers = new List<string> { "AAPL" };
+
+            var parameters = new List<AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam>
+                {
+                    new AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam("function", AlphaVantageApiWrapper.AlphaVantageApiWrapper.AvFuncationEnum.Sma.ToDescription()),
+                    new AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam("symbol", "AAPL"),
+                    new AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam("interval", AlphaVantageApiWrapper.AlphaVantageApiWrapper.AvIntervalEnum.Daily.ToDescription()),
+                    new AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam("time_period", "5"),
+                    new AlphaVantageApiWrapper.AlphaVantageApiWrapper.ApiParam("series_type", AlphaVantageApiWrapper.AlphaVantageApiWrapper.AvSeriesType.Open.ToDescription()),
+                };
+
+            var SMA_5 = await AlphaVantageApiWrapper.AlphaVantageApiWrapper.GetTechnical(parameters, API_KEY);
+            parameters.FirstOrDefault(x => x.ParamName == "time_period").ParamValue = "20";
+
+            ListViewItem listItem = new ListViewItem();
+            listItem.SubItems.Add(SMA_5.ToString());
+            listItem.SubItems.Add("test");
+            //listViewData.Columns.Add("testing");
+            listViewData.Items.Add(SMA_5.ToString());
+            listViewData.View = View.Details;
+
+        }
+
+        private void textBoxApiName_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
