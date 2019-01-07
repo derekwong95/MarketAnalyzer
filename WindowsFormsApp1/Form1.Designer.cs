1d6ef333f596156fb9a3e722fbe37b580b98482a
Jason Lao <Jasonlao1993@hotmail.com>
2019-01-06 23:19:54 -0700

    New form and graph function.

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,29 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.labelApiName = new System.Windows.Forms.Label();
             this.textBoxApiName = new System.Windows.Forms.TextBox();
             this.buttonSubmit = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPageGeneral = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.listViewRealTime = new System.Windows.Forms.ListView();
-            this.buttonSector = new System.Windows.Forms.Button();
-            this.listView1Day = new System.Windows.Forms.ListView();
-            this.listView5Day = new System.Windows.Forms.ListView();
-            this.listView1Month = new System.Windows.Forms.ListView();
-            this.listView3Month = new System.Windows.Forms.ListView();
-            this.listViewYearToDate = new System.Windows.Forms.ListView();
-            this.listView1Year = new System.Windows.Forms.ListView();
-            this.listView3Year = new System.Windows.Forms.ListView();
-            this.listView5Year = new System.Windows.Forms.ListView();
             this.listView10Year = new System.Windows.Forms.ListView();
-            this.columnHeaderRealTimeName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.columnHeaderRealTimeValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView5Year = new System.Windows.Forms.ListView();
+            this.listView3Year = new System.Windows.Forms.ListView();
+            this.listView1Year = new System.Windows.Forms.ListView();
+            this.listViewYearToDate = new System.Windows.Forms.ListView();
+            this.listView3Month = new System.Windows.Forms.ListView();
+            this.listView1Month = new System.Windows.Forms.ListView();
+            this.listView5Day = new System.Windows.Forms.ListView();
+            this.listView1Day = new System.Windows.Forms.ListView();
             this.columnHeader1DayName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeader1DayValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.buttonSector = new System.Windows.Forms.Button();
+            this.listViewRealTime = new System.Windows.Forms.ListView();
+            this.columnHeaderRealTimeName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderRealTimeValue = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.loadGraph = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPageGeneral.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelApiName
@@ -112,41 +120,69 @@
             this.tabPageGeneral.Text = "General";
             this.tabPageGeneral.UseVisualStyleBackColor = true;
             // 
-            // tabPage2
+            // listView10Year
             // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1231, 628);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.listView10Year.Location = new System.Drawing.Point(930, 294);
+            this.listView10Year.Name = "listView10Year";
+            this.listView10Year.Size = new System.Drawing.Size(225, 253);
+            this.listView10Year.TabIndex = 10;
+            this.listView10Year.UseCompatibleStateImageBehavior = false;
             // 
-            // listViewRealTime
+            // listView5Year
             // 
-            this.listViewRealTime.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.columnHeaderRealTimeName,
-            this.columnHeaderRealTimeValue});
-            this.listViewRealTime.FullRowSelect = true;
-            this.listViewRealTime.GridLines = true;
-            this.listViewRealTime.LabelWrap = false;
-            this.listViewRealTime.Location = new System.Drawing.Point(6, 35);
-            this.listViewRealTime.Name = "listViewRealTime";
-            this.listViewRealTime.Size = new System.Drawing.Size(225, 253);
-            this.listViewRealTime.TabIndex = 0;
-            this.listViewRealTime.TileSize = new System.Drawing.Size(225, 30);
-            this.listViewRealTime.UseCompatibleStateImageBehavior = false;
-            this.listViewRealTime.View = System.Windows.Forms.View.Details;
+            this.listView5Year.Location = new System.Drawing.Point(699, 294);
+            this.listView5Year.Name = "listView5Year";
+            this.listView5Year.Size = new System.Drawing.Size(225, 253);
+            this.listView5Year.TabIndex = 9;
+            this.listView5Year.UseCompatibleStateImageBehavior = false;
             // 
-            // buttonSector
+            // listView3Year
             // 
-            this.buttonSector.Location = new System.Drawing.Point(6, 6);
-            this.buttonSector.Name = "buttonSector";
-            this.buttonSector.Size = new System.Drawing.Size(390, 23);
-            this.buttonSector.TabIndex = 1;
-            this.buttonSector.Text = "Show Sector Information";
-            this.buttonSector.UseVisualStyleBackColor = true;
-            this.buttonSector.Click += new System.EventHandler(this.buttonSector_Click);
+            this.listView3Year.Location = new System.Drawing.Point(468, 294);
+            this.listView3Year.Name = "listView3Year";
+            this.listView3Year.Size = new System.Drawing.Size(225, 253);
+            this.listView3Year.TabIndex = 8;
+            this.listView3Year.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView1Year
+            // 
+            this.listView1Year.Location = new System.Drawing.Point(237, 294);
+            this.listView1Year.Name = "listView1Year";
+            this.listView1Year.Size = new System.Drawing.Size(225, 253);
+            this.listView1Year.TabIndex = 7;
+            this.listView1Year.UseCompatibleStateImageBehavior = false;
+            // 
+            // listViewYearToDate
+            // 
+            this.listViewYearToDate.Location = new System.Drawing.Point(6, 294);
+            this.listViewYearToDate.Name = "listViewYearToDate";
+            this.listViewYearToDate.Size = new System.Drawing.Size(225, 253);
+            this.listViewYearToDate.TabIndex = 6;
+            this.listViewYearToDate.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView3Month
+            // 
+            this.listView3Month.Location = new System.Drawing.Point(930, 35);
+            this.listView3Month.Name = "listView3Month";
+            this.listView3Month.Size = new System.Drawing.Size(225, 253);
+            this.listView3Month.TabIndex = 5;
+            this.listView3Month.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView1Month
+            // 
+            this.listView1Month.Location = new System.Drawing.Point(699, 35);
+            this.listView1Month.Name = "listView1Month";
+            this.listView1Month.Size = new System.Drawing.Size(225, 253);
+            this.listView1Month.TabIndex = 4;
+            this.listView1Month.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView5Day
+            // 
+            this.listView5Day.Location = new System.Drawing.Point(468, 35);
+            this.listView5Day.Name = "listView5Day";
+            this.listView5Day.Size = new System.Drawing.Size(225, 253);
+            this.listView5Day.TabIndex = 3;
+            this.listView5Day.UseCompatibleStateImageBehavior = false;
             // 
             // listView1Day
             // 
@@ -164,69 +200,41 @@
             this.listView1Day.UseCompatibleStateImageBehavior = false;
             this.listView1Day.View = System.Windows.Forms.View.Details;
             // 
-            // listView5Day
+            // columnHeader1DayName
             // 
-            this.listView5Day.Location = new System.Drawing.Point(468, 35);
-            this.listView5Day.Name = "listView5Day";
-            this.listView5Day.Size = new System.Drawing.Size(225, 253);
-            this.listView5Day.TabIndex = 3;
-            this.listView5Day.UseCompatibleStateImageBehavior = false;
+            this.columnHeader1DayName.Text = "Name";
+            this.columnHeader1DayName.Width = 100;
             // 
-            // listView1Month
+            // columnHeader1DayValue
             // 
-            this.listView1Month.Location = new System.Drawing.Point(699, 35);
-            this.listView1Month.Name = "listView1Month";
-            this.listView1Month.Size = new System.Drawing.Size(225, 253);
-            this.listView1Month.TabIndex = 4;
-            this.listView1Month.UseCompatibleStateImageBehavior = false;
+            this.columnHeader1DayValue.Text = "Value";
+            this.columnHeader1DayValue.Width = 120;
             // 
-            // listView3Month
+            // buttonSector
             // 
-            this.listView3Month.Location = new System.Drawing.Point(930, 35);
-            this.listView3Month.Name = "listView3Month";
-            this.listView3Month.Size = new System.Drawing.Size(225, 253);
-            this.listView3Month.TabIndex = 5;
-            this.listView3Month.UseCompatibleStateImageBehavior = false;
+            this.buttonSector.Location = new System.Drawing.Point(6, 6);
+            this.buttonSector.Name = "buttonSector";
+            this.buttonSector.Size = new System.Drawing.Size(390, 23);
+            this.buttonSector.TabIndex = 1;
+            this.buttonSector.Text = "Show Sector Information";
+            this.buttonSector.UseVisualStyleBackColor = true;
+            this.buttonSector.Click += new System.EventHandler(this.buttonSector_Click);
             // 
-            // listViewYearToDate
+            // listViewRealTime
             // 
-            this.listViewYearToDate.Location = new System.Drawing.Point(6, 294);
-            this.listViewYearToDate.Name = "listViewYearToDate";
-            this.listViewYearToDate.Size = new System.Drawing.Size(225, 253);
-            this.listViewYearToDate.TabIndex = 6;
-            this.listViewYearToDate.UseCompatibleStateImageBehavior = false;
-            // 
-            // listView1Year
-            // 
-            this.listView1Year.Location = new System.Drawing.Point(237, 294);
-            this.listView1Year.Name = "listView1Year";
-            this.listView1Year.Size = new System.Drawing.Size(225, 253);
-            this.listView1Year.TabIndex = 7;
-            this.listView1Year.UseCompatibleStateImageBehavior = false;
-            // 
-            // listView3Year
-            // 
-            this.listView3Year.Location = new System.Drawing.Point(468, 294);
-            this.listView3Year.Name = "listView3Year";
-            this.listView3Year.Size = new System.Drawing.Size(225, 253);
-            this.listView3Year.TabIndex = 8;
-            this.listView3Year.UseCompatibleStateImageBehavior = false;
-            // 
-            // listView5Year
-            // 
-            this.listView5Year.Location = new System.Drawing.Point(699, 294);
-            this.listView5Year.Name = "listView5Year";
-            this.listView5Year.Size = new System.Drawing.Size(225, 253);
-            this.listView5Year.TabIndex = 9;
-            this.listView5Year.UseCompatibleStateImageBehavior = false;
-            // 
-            // listView10Year
-            // 
-            this.listView10Year.Location = new System.Drawing.Point(930, 294);
-            this.listView10Year.Name = "listView10Year";
-            this.listView10Year.Size = new System.Drawing.Size(225, 253);
-            this.listView10Year.TabIndex = 10;
-            this.listView10Year.UseCompatibleStateImageBehavior = false;
+            this.listViewRealTime.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderRealTimeName,
+            this.columnHeaderRealTimeValue});
+            this.listViewRealTime.FullRowSelect = true;
+            this.listViewRealTime.GridLines = true;
+            this.listViewRealTime.LabelWrap = false;
+            this.listViewRealTime.Location = new System.Drawing.Point(6, 35);
+            this.listViewRealTime.Name = "listViewRealTime";
+            this.listViewRealTime.Size = new System.Drawing.Size(225, 253);
+            this.listViewRealTime.TabIndex = 0;
+            this.listViewRealTime.TileSize = new System.Drawing.Size(225, 30);
+            this.listViewRealTime.UseCompatibleStateImageBehavior = false;
+            this.listViewRealTime.View = System.Windows.Forms.View.Details;
             // 
             // columnHeaderRealTimeName
             // 
@@ -238,15 +246,51 @@
             this.columnHeaderRealTimeValue.Text = "Value";
             this.columnHeaderRealTimeValue.Width = 120;
             // 
-            // columnHeader1DayName
+            // tabPage2
             // 
-            this.columnHeader1DayName.Text = "Name";
-            this.columnHeader1DayName.Width = 100;
+            this.tabPage2.Controls.Add(this.loadGraph);
+            this.tabPage2.Controls.Add(this.chart1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1231, 628);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
-            // columnHeader1DayValue
+            // chart1
             // 
-            this.columnHeader1DayValue.Text = "Value";
-            this.columnHeader1DayValue.Width = 120;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.DockedToChartArea = "ChartArea1";
+            legend1.Name = "Apple";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(27, 25);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Legend = "Apple";
+            series1.Name = "Apple";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(737, 524);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            title1.BackColor = System.Drawing.Color.Transparent;
+            title1.BorderWidth = 10;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            title1.Name = "Stock Price";
+            title1.Text = "Stock Price";
+            this.chart1.Titles.Add(title1);
+            // 
+            // loadGraph
+            // 
+            this.loadGraph.Location = new System.Drawing.Point(838, 122);
+            this.loadGraph.Name = "loadGraph";
+            this.loadGraph.Size = new System.Drawing.Size(255, 40);
+            this.loadGraph.TabIndex = 1;
+            this.loadGraph.Text = "Load Graph";
+            this.loadGraph.UseVisualStyleBackColor = true;
+            this.loadGraph.Click += new System.EventHandler(this.loadGraph_Click);
             // 
             // Form1
             // 
@@ -262,6 +306,8 @@
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPageGeneral.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -274,7 +320,6 @@
         private System.Windows.Forms.Button buttonSubmit;
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tabPageGeneral;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.ListView listViewRealTime;
         private System.Windows.Forms.ListView listView10Year;
         private System.Windows.Forms.ListView listView5Year;
@@ -290,6 +335,9 @@
         private System.Windows.Forms.ColumnHeader columnHeaderRealTimeValue;
         private System.Windows.Forms.ColumnHeader columnHeader1DayName;
         private System.Windows.Forms.ColumnHeader columnHeader1DayValue;
+        public System.Windows.Forms.Button loadGraph;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        public System.Windows.Forms.TabPage tabPage2;
     }
 }
 
